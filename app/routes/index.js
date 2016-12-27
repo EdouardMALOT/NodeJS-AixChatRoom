06db2d0c25dfe3b674339c3d43449f6338790642
Edouard <edouard.malot@gmail.com>
2016-12-27 17:29:56 +0700

    =Add 404 route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,26 @@
 'use strict';
 const router = require('express').Router();
 
+//Standards routes
+//----------------
 router
         .get('/', (req, res, next) => {
             res.render('login');
         })
 
         .get('/rooms', (req, res, next) => {
-        res.render('rooms');
+            res.render('rooms');
         })
 
         .get('/chat', (req, res, next) => {
-        res.render('chatroom');
+            res.render('chatroom');
         });
+
+//Default router
+//--------------
+router.use( (req, res, next) => {
+            res.status(404);
+            res.sendfile(process.cwd() + '/views/404.htm');
+         });
 
 module.exports = { router };
