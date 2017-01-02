7ce8ee12d038c316942d859da3a809759988ca1b
Edouard <edouard.malot@gmail.com>
2017-01-02 22:27:15 +0700

    =bug correction

--- app/config/index.js
+++ app/config/index.js
@@ -2,8 +2,8 @@
 
 if(process.env.NODE_ENV === 'production') {
 
-    let redisURI = require('url').parse(process.env.REDIS_URL);
-    let redisPassword = redisURI.auth.split(':')[1];
+    //let redisURI = require('url').parse(process.env.REDIS_URL);
+    //let redisPassword = redisURI.auth.split(':')[1];
 
     module.exports = {
         host: process.env.host || "",
@@ -14,12 +14,12 @@ if(process.env.NODE_ENV === 'production') {
             clientSecret: process.env.fbClientSecret,
             callbackURL: process.env.host + "/auth/facebook/callback",
             profileFields: ["id", "displayName", "photos"]
-        },
-        redis : {
-            host: redisURI.hostname,
-            port: redisURI.port,
-            password: redisPassword
         }
+        //redis : {
+        //    host: redisURI.hostname,
+        //    port: redisURI.port,
+        //    password: redisPassword
+        //}
     };
     
 }else{
